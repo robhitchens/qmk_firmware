d85ec57d3314f4c9098cf36864aae66292ce3ab6
Robert Hitchens <rjhitchens3@gmail.com>
2020-05-31 17:21:54 -0400

    forgot to swap lctrl and esc on qwerty layer

--- keyboards/planck/keymaps/robhitchens-singlespace/keymap.c
+++ keyboards/planck/keymaps/robhitchens-singlespace/keymap.c
@@ -58,9 +58,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_QWERTY] = LAYOUT_planck_grid(
      KC_TAB,    KC_Q,    KC_W,  KC_E,  KC_R,     KC_T,     KC_Y,  KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
-     KC_ESC,    KC_A,    KC_S,  KC_D,  KC_F,     KC_G,     KC_H,  KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
+    KC_LCTL,    KC_A,    KC_S,  KC_D,  KC_F,     KC_G,     KC_H,  KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
     KC_LSFT,    KC_Z,    KC_X,  KC_C,  KC_V,     KC_B,     KC_N,  KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ENT,
-    KC_LCTL, KC_LGUI, KC_LALT, FUNCK, LOWER, TAPSPACE, TAPSPACE, RAISE, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT
+     KC_ESC, KC_LGUI, KC_LALT, FUNCK, LOWER, TAPSPACE, TAPSPACE, RAISE, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT
 ),
 [_SPACEFN] = LAYOUT_planck_grid(
        KC_TAB, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,  KC_GRV,
