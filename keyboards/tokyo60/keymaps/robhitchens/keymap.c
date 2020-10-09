28b0b52d359ae9e7e0e29d6141b919a116c0e58e
Robert Hitchens <rjhitchens3@gmail.com>
2020-10-09 18:21:22 -0400

    - updated tokyo60 keymap layout

--- keyboards/tokyo60/keymaps/robhitchens/keymap.c
+++ keyboards/tokyo60/keymaps/robhitchens/keymap.c
@@ -23,15 +23,13 @@ enum tokyo60_keycodes {
     BASE = SAFE_RANGE,
     GAMEMODE,
     SPACEFN,
-    RAISE,
-    LOWER,
-    FUNCK,
     ALT_TAB,
     ALSFH_TAB
 };
 
 #define RAISE MO(_RAISE)
 #define LOWER MO(_LOWER)
+#define FUNCK MO(_FUNCK)
 #define SPACEFN MO(_SPACEFN)
 #define TAPSPACE LT(SPACEFN, KC_SPC)
 #define GUISPACE LT(SPACEFN, KC_RGUI)
@@ -66,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC,
         KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, GUISPACE,
-        KC_LALT, LOWER, /*        */ KC_SPC, RAISE, KC_RALT
+        LOWER, FUNCK, /*        */ KC_SPC, RAISE, KC_RALT
     ),
     [_SPACEFN] = LAYOUT_60_hhkb(
         KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_PIPE, KC_GRV,
@@ -104,7 +102,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, /*        */ _______, _______, _______
     ),
+    [_FUNCK] = LAYOUT_60_hhkb( //  default layer
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, KC_MS_WH_UP, _______, KC_MS_WH_DOWN, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, /*        */ _______, _______, _______
+    ),
 };
+
+
 
 layer_state_t layer_state_set_user(layer_state_t state){
     state = update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
