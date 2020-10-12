789dc951633b90c3fc1a3509207e035e798b41de
Robert Hitchens <rjhitchens3@gmail.com>
2020-10-12 18:27:45 -0400

    - updated planck and jj40 keyboard layouts

--- keyboards/jj40/keymaps/robhitchens/keymap.c
+++ keyboards/jj40/keymaps/robhitchens/keymap.c
@@ -30,7 +30,8 @@ enum layers {
   _SHIFTL,
   _PLOVER,
   _ADJUST,
-  _FUNCK
+  _FUNCK,
+  _SPECL
 };
 
 enum keycodes {
@@ -49,6 +50,7 @@ enum keycodes {
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
 #define FUNCK MO(_FUNCK)
+#define SPECL MO(_SPECL)
 #define SPACEFN MO(_SPACEFN)
 #define HOMENUM MO(_HOMENUM)
 //#define TAPLEFTARROW LT(LEFTARROWFN, KC_LEFT)
@@ -62,11 +64,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LSFT,    KC_Z,    KC_X,  KC_C,  KC_V,     KC_B,     KC_N,  KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ENT,
      KC_ESC, KC_LGUI, KC_LALT, FUNCK, LOWER, TAPSPACE, RAISE, HOMENUM, KC_RALT,   KC_RGUI, KC_ESC
 ),
-[_GAMEMODE] = LAYOUT_planck_mit(
+[_GAMEMODE] = LAYOUT_planck_grid(
      KC_TAB,     KC_Q,    KC_W,  KC_E,  KC_R,     KC_T,     KC_Y,  KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
     KC_LCTL,     KC_A,    KC_S,  KC_D,  KC_F,     KC_G,     KC_H,  KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
     KC_LSFT,     KC_Z,    KC_X,  KC_C,  KC_V,     KC_B,     KC_N,  KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ENT,
-     KC_ESC, GUISPACE, KC_LALT, FUNCK, LOWER,   KC_SPC,    RAISE, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT),
+     KC_ESC, GUISPACE, FUNCK, SPECL, LOWER,   KC_SPC,   KC_SPC, RAISE, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT),
 [_SPACEFN] = LAYOUT_planck_mit(
        KC_TAB, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,  KC_GRV,
       KC_LCTL, XXXXXXX,  KC_GRV, KC_MINS,  KC_EQL, KC_BSLS, KC_PIPE, KC_LBRC, KC_RBRC, XXXXXXX, KC_COLN, KC_DQUO,
@@ -76,6 +78,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_HOMENUM] = LAYOUT_planck_mit(
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
       _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, _______,
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+),
+[_SPECL] = LAYOUT_planck_mit(
+      _______, KC_MS_WH_DOWN, _______, KC_MS_WH_UP, _______, _______, _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
--- keyboards/planck/keymaps/robhitchens-singlespace/keymap.c
+++ keyboards/planck/keymaps/robhitchens-singlespace/keymap.c
@@ -33,17 +33,13 @@ enum planck_layers {
   _SHIFTL,
   _PLOVER,
   _ADJUST,
-  _FUNCK
+  _FUNCK,
+  _SPECL
 };
 
 enum planck_keycodes {
   QWERTY = SAFE_RANGE,
-  //LOWER,
-  //RAISE,
   GAMEMODE,
-  //SPACEFN,
-  //LEFTARROWFN,
-  //SHIFTL,
   PLOVER,
   BACKLIT,
   EXT_PLV,
@@ -56,6 +52,7 @@ enum planck_keycodes {
 #define RAISE MO(_RAISE)
 #define SHIFTL MO(_SHIFTL)
 #define FUNCK MO(_FUNCK)
+#define SPECL MO(_SPECL)
 #define SPACEFN MO(_SPACEFN)
 #define TAPSPACE LT(SPACEFN, KC_SPC)
 #define GUISPACE LT(SPACEFN, KC_LGUI)
@@ -85,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB,     KC_Q,    KC_W,  KC_E,  KC_R,     KC_T,     KC_Y,  KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
     KC_LCTL,     KC_A,    KC_S,  KC_D,  KC_F,     KC_G,     KC_H,  KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
     KC_LSFT,     KC_Z,    KC_X,  KC_C,  KC_V,     KC_B,     KC_N,  KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ENT,
-     KC_ESC, GUISPACE, KC_LALT, FUNCK, LOWER,   KC_SPC,   KC_SPC, RAISE, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT),
+     KC_ESC, GUISPACE, FUNCK, SPECL, LOWER,   KC_SPC,   KC_SPC, RAISE, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT),
 [_SPACEFN] = LAYOUT_planck_grid(
        KC_TAB, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,  KC_GRV,
       KC_LCTL, XXXXXXX,  KC_GRV, KC_MINS,  KC_EQL, KC_BSLS, KC_PIPE, KC_LBRC, KC_RBRC, XXXXXXX, KC_COLN, KC_DQUO,
@@ -95,6 +92,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_HOMENUM] = LAYOUT_planck_mit(
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
       _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, _______,
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+),
+[_SPECL] = LAYOUT_planck_mit(
+      _______, KC_MS_WH_DOWN, _______, KC_MS_WH_UP, _______, _______, _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
