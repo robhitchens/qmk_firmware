b8ba88e41032dfeaa386c0074baea420804c4b88
Robert Hitchens <rjhitchens3@gmail.com>
2020-11-03 13:27:46 -0500

    - update layout for tkc1800

--- keyboards/tkc/tkc1800/keymaps/robhitchens/keymap.c
+++ keyboards/tkc/tkc1800/keymaps/robhitchens/keymap.c
@@ -102,19 +102,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_LOWER] = LAYOUT(
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                    _______,  _______, _______, _______, \
                                                                                                                                             _______,  _______, _______, _______, \
-    KC_ESC,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PIPE, XXXXXXX, _______,  _______, _______, _______, \
-    _______,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_LCBR, KC_RCBR, _______,          _______,  _______, _______, _______, \
-    _______,  XXXXXXX, KC_GRV,  KC_MINS, KC_EQL,  KC_BSLS, KC_PIPE, KC_LBRC, KC_RCBR, XXXXXXX, KC_COLN, KC_DQUO, XXXXXXX, _______,          _______,  _______, _______, _______, \
-    _______,  XXXXXXX, XXXXXXX, KC_TILD, KC_UNDS, KC_PLUS, XXXXXXX, KC_LCBR, KC_RCBR, KC_LT,   KC_GT,   KC_QUES, _______,         KC_UP,    _______,  _______, _______, XXXXXXX, \
+    _______,  _______,       _______,  _______,  _______,   _______,  _______,  _______,  _______, _______,  _______, _______,  _______,  _______, XXXXXXX, _______,  _______, _______, _______, \
+    _______,  KC_MS_WH_LEFT, KC_MS_U,  KC_MS_WH_RIGHT, _______,  _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,  _______, _______, _______, \
+    _______,  KC_MS_L,       KC_MS_D,  KC_MS_R, _______,  _______, _______, KC_MS_BTN1, KC_MS_BTN2, _______, _______, _______, XXXXXXX, _______,          _______,  _______, _______, _______, \
+    _______,  XXXXXXX, KC_MS_WH_DOWN,  KC_MS_BTN3, KC_MS_WH_UP, _______, _______, _______, _______, _______, _______, _______, _______,         KC_UP,    _______,  _______, _______, XXXXXXX, \
     _______,  _______, _______,                   _______,                               XXXXXXX, _______, _______,      KC_LEFT, KC_DOWN, KC_RGHT,      _______, _______, _______  \
   ),
   [_RAISE] = LAYOUT(
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                    _______,  _______, _______, _______, \
                                                                                                                                             _______,  _______, _______, _______, \
-    KC_ESC,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PIPE, XXXXXXX, _______,  _______, _______, _______, \
-    _______,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_LCBR, KC_RCBR, _______,          _______,  _______, _______, _______, \
-    _______,  XXXXXXX, KC_GRV,  KC_MINS, KC_EQL,  KC_BSLS, KC_PIPE, KC_LBRC, KC_RCBR, XXXXXXX, KC_COLN, KC_DQUO, XXXXXXX, _______,          _______,  _______, _______, _______, \
-    _______,  XXXXXXX, XXXXXXX, KC_TILD, KC_UNDS, KC_PLUS, XXXXXXX, KC_LCBR, KC_RCBR, KC_LT,   KC_GT,   KC_QUES, _______,         KC_UP,    _______,  _______, _______, XXXXXXX, \
+    _______,  _______,       _______,  _______,  _______,   _______,  _______,  _______,  _______, _______,  _______, _______,  _______,  _______, XXXXXXX, _______,  _______, _______, _______, \
+    _______,  ALSFH_TAB, KC_UP,  ALT_TAB, _______,  _______, _______, KC_PSCR, KC_INS, KC_HOME, KC_PG_UP, _______, _______, _______,          _______,  _______, _______, _______, \
+    _______,  KC_LEFT,   KC_DOWN,KC_RIGHT, _______,  KC_CAPS, KC_ESC, KC_APP, _______, KC_END, KC_PGDN, _______, XXXXXXX, _______,          _______,  _______, _______, _______, \
+    _______,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, _______, _______, _______,   _______,   _______, _______,         KC_UP,    _______,  _______, _______, XXXXXXX, \
+    _______,  _______, _______,                   _______,                               XXXXXXX, _______, _______,      KC_LEFT, KC_DOWN, KC_RGHT,      _______, _______, _______  \
+  ),
+  [_SHIFTL] = LAYOUT(
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                    _______,  _______, _______, _______, \
+                                                                                                                                            _______,  _______, _______, _______, \
+    _______,  _______,       _______,  _______,  _______,   _______,  _______,  _______,  _______, _______,  _______, _______,  _______,  _______, XXXXXXX, _______,  _______, _______, _______, \
+    _______,  S(KC_Q), S(KC_W),  S(KC_E),   S(KC_R),    S(KC_T),    S(KC_Y),    S(KC_U),    S(KC_I),    S(KC_O),    S(KC_P),    S(KC_LBRC), S(KC_RBRC), _______,         _______,  _______,  _______, _______, \
+    _______,  S(KC_A), S(KC_S),  S(KC_D),   S(KC_F),    S(KC_G),    S(KC_H),    S(KC_J),    S(KC_K),    S(KC_L),    S(KC_SCLN), S(KC_QUOT), XXXXXXX, _______,          _______,  _______,  _______, _______, \
+    _______,  XXXXXXX, S(KC_Z),  S(KC_X),   S(KC_C),    S(KC_V),    S(KC_B),    S(KC_N),    S(KC_M),    S(KC_COMM), S(KC_DOT),  S(KC_SLSH), _______,      KC_UP,      _______,   _______,  _______,   XXXXXXX, \
+    _______,  _______, _______,                   _______,                               XXXXXXX, _______, _______,      KC_LEFT, KC_DOWN, KC_RGHT,      _______, _______, _______  \
+  ),
+  [_ADJUST] = LAYOUT(
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                    _______,  _______, _______, _______, \
+                                                                                                                                            _______,  _______, _______, _______, \
+    _______,  _______,       _______,  _______,  _______,   _______,  _______,  _______,  _______, _______,  _______, _______,  _______,  _______, XXXXXXX, _______,  _______, _______, _______, \
+    _______,  RESET, DEBUG,  _______,   _______,    _______,    _______,    _______,  _______, _______,  _______, _______, _______, _______,         _______,  _______,  _______, _______, \
+    _______,  BL_TOGGL, BL_STEP,  BL_ON,  BL_OFF, BL_INC, BL_DEC, BL_BRTG,  _______,  _______, _______, _______, XXXXXXX, _______,          _______,  _______,  _______, _______, \
+    _______,  XXXXXXX, _______,  _______,   _______,    _______,    _______,    _______,    _______,    _______, _______,  _______, _______,      KC_UP,      _______,   _______,  _______,   XXXXXXX, \
     _______,  _______, _______,                   _______,                               XXXXXXX, _______, _______,      KC_LEFT, KC_DOWN, KC_RGHT,      _______, _______, _______  \
   ),
   /* Keymap FUNCTION: (Function Layer)
@@ -134,11 +152,98 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |    |    |    |                      |    |   |   |     |    |    |    | .  |    |
    * `---------------------------------------------------------------------------------'
    */
-  [FUNCTION] = LAYOUT(
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, \
-                                                                                                                                           _______, _______, _______, _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET,   XXXXXXX, _______, _______, _______, _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, \
+};
+
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+  state = update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+  state = update_tri_layer_state(state, _SPACEFN, _LOWER, _SHIFTL);
+  return state;
+}
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case ALT_TAB:
+        if(record->event.pressed){
+            if(!is_alt_tab_active){
+                is_alt_tab_active = true;
+                register_code(KC_LALT);
+            }
+            alt_tab_timer = timer_read();
+            register_code(KC_TAB);
+        }else{
+            unregister_code(KC_TAB);
+        }
+        break;
+    case ALSFH_TAB:
+        if(record->event.pressed){
+            if(!is_shift_alt_tab_active){
+                is_shift_alt_tab_active = true;
+                register_code(KC_LALT);
+                register_code(KC_LSHIFT);
+            }
+            shift_alt_tab_timer = timer_read();
+            register_code(KC_TAB);
+        }else{
+            unregister_code(KC_TAB);
+        }
+        break;
+    case BASE:
+      if (record->event.pressed) {
+        print("mode just switched to qwerty and this is a huge string\n");
+        set_single_persistent_default_layer(_BASE);
+      }
+      return false;
+      break;
+    case GAMEMODE:
+      if(record->event.pressed){
+        print("mode just switched to gamemode");
+        set_single_persistent_default_layer(_GAMEMODE);
+      }
+      return false;
+      break;
+      /*
+    case BACKLIT:
+      if (record->event.pressed) {
+        register_code(KC_RSFT);
+        #ifdef BACKLIGHT_ENABLE
+          backlight_step();
+        #endif
+        #ifdef KEYBOARD_planck_rev5
+          writePinLow(E6);
+        #endif
+      } else {
+        unregister_code(KC_RSFT);
+        #ifdef KEYBOARD_planck_rev5
+          writePinHigh(E6);
+        #endif
+      }
+      return false;
+      break;*/
+  }
+  return true;
+}
+
+void matrix_scan_user(void) {
+    if(is_alt_tab_active){
+        if(timer_elapsed(alt_tab_timer) > 500){
+            unregister_code(KC_LALT);
+            is_alt_tab_active = false;
+        }
+    }
+    if(is_shift_alt_tab_active){
+        if(timer_elapsed(shift_alt_tab_timer) > 500){
+            unregister_code(KC_LALT);
+            unregister_code(KC_LSHIFT);
+            is_shift_alt_tab_active = false;
+        }
+    }
+}
+
+
+#ifdef OLED_DRIVER_ENABLE
+void oled_task_user(void) {
+    oled_write_P(PSTR("TKC1800\n"),false);
+	// Host Keyboard Layer Status
     oled_write_P(PSTR("Layer: "), false);
 
     switch (get_highest_layer(layer_state)) {
