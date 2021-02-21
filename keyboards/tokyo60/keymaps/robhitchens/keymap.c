0925b3b9c6261cb31a55152a45c1e1a901645518
Rob Hitchens <rjhitchens3@gmail.com>
2021-02-21 10:30:41 -0500

    - swapped raise and lower layers on tokyo60

--- keyboards/tokyo60/keymaps/robhitchens/keymap.c
+++ keyboards/tokyo60/keymaps/robhitchens/keymap.c
@@ -57,14 +57,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC,
         KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_RGUI,
-        KC_LALT, LOWER, /*        */ TAPSPACE, RAISE, KC_RALT
+        KC_LALT, RAISE, /*        */ TAPSPACE, LOWER, KC_RALT
     ),
     [_GAMEMODE] = LAYOUT_60_hhkb( //  default layer
         KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSLS, KC_GRV,
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC,
         KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, GUISPACE,
-        LOWER, FUNCK, /*        */ KC_SPC, RAISE, KC_RALT
+        RAISE, FUNCK, /*        */ KC_SPC, LOWER, KC_RALT
     ),
     [_SPACEFN] = LAYOUT_60_hhkb(
         KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_PIPE, KC_GRV,
@@ -115,7 +115,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 layer_state_t layer_state_set_user(layer_state_t state){
     state = update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
-    state = update_tri_layer_state(state, _SPACEFN, _LOWER, _SHIFTL);
+    state = update_tri_layer_state(state, _SPACEFN, _RAISE, _SHIFTL);
     return state;
 }
 
