d2b2e2ed328dba67c81c8782f67e943c46bc12f8
Rob Hitchens <rjhitchens3@gmail.com>
2021-04-06 18:35:57 -0400

    - update to boardwalk layout

--- keyboards/boardwalk/keymaps/robhitchens/keymap.c
+++ keyboards/boardwalk/keymaps/robhitchens/keymap.c
@@ -65,20 +65,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    KC_LEFT,       KC_RIGHT,    KC_Y,    KC_U,  KC_I,    KC_O,    KC_P,     KC_BSPC, \
   KC_LCTL, KC_A,     KC_S,    KC_D,    KC_F,    KC_G,    KC_DOWN,       KC_UP,       KC_H,    KC_J,  KC_K,    KC_L,    KC_SCLN,  KC_ENT,  \
   KC_LSFT, KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,    KC_DQUO,       KC_QUOTE, KC_N,    KC_M,  KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
-  KC_LALT, GUISPACE, KC_LALT, LOWER,   FUNCK,   KC_SPC,  KC_SPC,        KC_RSFT,     KC_RSFT, RAISE, FUNCK,   KC_RALT, GUISPACE, KC_RALT  \
+  KC_LALT, GUISPACE, FUNCK,KC_LALT,   LOWER,   KC_SPC,  KC_SPC,        KC_RSFT,     KC_RSFT, RAISE, FUNCK,   KC_RALT, GUISPACE, KC_RALT  \
 ),
 [_SPACEFN] = LAYOUT_ortho_5x14(
   KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F11,  KC_F12,  KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10, KC_GRV, \
   KC_TAB,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______, _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, \
-  KC_LCTL, XXXXXXX, KC_GRV,  KC_MINS, KC_EQL,  KC_BSLS, _______, _______, KC_PIPE, KC_LBRC, KC_RBRC, XXXXXXX, KC_COLN, KC_DQUO,  \
-  KC_LSFT, XXXXXXX, KC_TILD, KC_UNDS, KC_PLUS, XXXXXXX, _______, _______, KC_LCBR, KC_RCBR, KC_LT,   KC_GT,   KC_QUES, KC_ENT, \
+  KC_LCTL, XXXXXXX, KC_GRV,  KC_MINS, KC_EQL,  KC_BSLS, _______, _______, KC_PIPE, KC_LBRC, KC_RBRC, XXXXXXX, KC_COLN, _______,  \
+  KC_LSFT, XXXXXXX, KC_TILD, KC_UNDS, KC_PLUS, XXXXXXX, _______, KC_DQUO, KC_LCBR, KC_RCBR, KC_LT,   KC_GT,   KC_QUES, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
 ),
 [_LOWER] = LAYOUT_ortho_5x14(
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  \
-  _______, KC_MS_WH_LEFT,   KC_MS_U,KC_MS_WH_RIGHT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+  _______, KC_MS_WH_UP,   KC_MS_U,KC_MS_WH_DOWN, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
   _______, KC_MS_L,    KC_MS_D,    KC_MS_R, _______, _______, _______, _______, _______, KC_MS_BTN1, KC_MS_BTN2, _______, _______, _______,  \
-  _______, KC_MS_WH_UP, KC_MS_BTN3, KC_MS_WH_DOWN, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+  _______, KC_MS_WH_LEFT, KC_MS_BTN3, KC_MS_WH_RIGHT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
 ),
 [_RAISE] = LAYOUT_ortho_5x14(
