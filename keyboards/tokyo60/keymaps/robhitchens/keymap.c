ea39a6ae035eded1aba5e26c3a8f9472c0f686ff
Rob Hitchens <rjhitchens3@gmail.com>
2021-04-29 19:54:06 -0400

    - updated lower layout for tokyo60

--- keyboards/tokyo60/keymaps/robhitchens/keymap.c
+++ keyboards/tokyo60/keymaps/robhitchens/keymap.c
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC,
         KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
         KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, GUISPACE,
-        RAISE, FUNCK, /*        */ KC_SPC, LOWER, KC_RALT
+        RAISE, LOWER, /*        */ KC_SPC, RAISE, KC_RALT
     ),
     [_SPACEFN] = LAYOUT_60_hhkb(
         KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_PIPE, KC_GRV,
@@ -79,9 +79,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [_LOWER] = LAYOUT_60_hhkb( //  default layer
         _______,       _______,    _______,        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, KC_MS_WH_LEFT,    KC_MS_U, KC_MS_WH_RIGHT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, KC_MS_WH_UP,    KC_MS_U, KC_MS_WH_DOWN, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______,       KC_MS_L,    KC_MS_D,        KC_MS_R, _______, _______, _______, KC_MS_BTN1, KC_MS_BTN2, _______, _______, _______, _______,
-        _______, KC_MS_WH_UP, KC_MS_BTN3,    KC_MS_WH_DOWN, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, KC_MS_WH_LEFT, KC_MS_BTN3,    KC_MS_WH_RIGHT, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, /*        */ _______, _______, _______
     ),
     [_RAISE] = LAYOUT_60_hhkb( //  default layer
