aeda8467133a93a10c1fcc4aee780f706099b1c1
Rob Hitchens <rjhitchens3@gmail.com>
2021-07-16 22:08:34 -0400

    - updated persistent layers on m50-a

--- keyboards/wilba_tech/rama_works_m50_a/keymaps/robhitchens/keymap.c
+++ keyboards/wilba_tech/rama_works_m50_a/keymaps/robhitchens/keymap.c
@@ -62,8 +62,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 [_GAMEMODE] = LAYOUT(
     KC_MS_WH_UP,   KC_TAB,     KC_Q,    KC_W,  KC_E,  KC_R,     KC_T,     KC_Y,  KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
-    KC_MS_WH_DOWN, KC_LCTL,     KC_A,    KC_S,  KC_D,  KC_F,     KC_G,     KC_H,  KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
-    KC_MS_BTN1,    KC_LSFT,     KC_Z,    KC_X,  KC_C,  KC_V,     KC_B,     KC_N,  KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ENT,
+    KC_MS_WH_DOWN, KC_LCTL,     KC_A,    KC_S,  KC_D,  KC_F,     KC_G,     KC_H,  KC_J,    KC_K,    KC_L, KC_SCLN, KC_ENT,
+    KC_MS_BTN1,    KC_LSFT,     KC_Z,    KC_X,  KC_C,  KC_V,     KC_B,     KC_N,  KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
     KC_MS_BTN2,    KC_ESC, GUISPACE, FUNCK, FUNCK, LOWER,   KC_SPC, RAISE, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT
 ),
 [_SPACEFN] = LAYOUT(
